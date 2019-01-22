7ba7a8844bb033221deae8c3ae9d31fdffec5bd0
Winston Smith <winstonsmithcodes@gmail.com>
2019-01-22 13:39:11 +0700

    update calltoaction functionality

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './style.scss';
 
-const CallToAction = props => (
-  <section className="call-to-action">
-    <div className="section-content">
-      <h1>Get early access today</h1>
-      <p>
-        It only takes a minute to sign up and our free starter tier is extremely generous. If you have any
-        questions, our support team would be happy to help you.
-      </p>
-      <input type="email" name="" value="" />
-      <button type="button" name="button" className="get-started">Get Started For Free</button>
-    </div>
-  </section>
-);
+const CallToAction = props => {
 
+  const {bkgSecondary, bkgMain, bkgFooter, fontBody} = props.theme;
+  const sectionStyle = {
+      backgroundImage: `linear-gradient(to bottom,  ${bkgMain} 0%,${bkgMain} 50%,${bkgFooter} 50%,${bkgFooter} 100%)`,
+      color: fontBody
+  }
+  const contentStyle = {
+    backgroundColor: bkgSecondary
+  }
+
+  return(
+    <section className="call-to-action" style={sectionStyle}>
+      <div className="section-content" style={contentStyle}>
+        <h1>Get early access today</h1>
+        <p>
+          It only takes a minute to sign up and our free starter tier is extremely generous. If you have any
+          questions, our support team would be happy to help you.
+        </p>
+        <input type="email" name="" value="" />
+        <button type="button" name="button" className="get-started">Get Started For Free</button>
+      </div>
+    </section>
+  );
+}
 export default CallToAction;
 // TODO: Ensure mobile css
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -5,7 +5,7 @@ import TestimonialSquare from './components/TestimonialSquare';
 import Content from "./content";
 
 const Testimonials = props => {
-  const {bkgMain, bkgAddtl, fontBody} = props.theme;
+  const {bkgMain, fontBody} = props.theme;
   const {theme} = props;
   const sectionStyle = {
     backgroundColor: bkgMain,
@@ -28,31 +28,3 @@ const Testimonials = props => {
 export default Testimonials;
 // TODO: Ensure mobile css
 // TODO: Create testimonial components
-
-
-// <div className="testimonial-square">
-// <p>
-// Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has
-// become a well-oiled collaboration machine.
-// </p>
-// <div className="testimonial-profile">
-// <img src={require("./images/profile-2.jpg")} alt="" />
-// <div className="author">
-// <h4>Bruce McKenzie</h4>
-// <p>Founder & CEO, Huddle</p>
-// </div>
-// </div>
-// </div>
-// <div className="testimonial-square">
-// <p>
-// Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has
-// become a well-oiled collaboration machine.
-// </p>
-// <div className="testimonial-profile">
-// <img src={require("./images/profile-3.jpg")} alt="" />
-// <div className="author">
-// <h4>Iva Boyd</h4>
-// <p>Founder & CEO, Huddle</p>
-// </div>
-// </div>
-// </div>
